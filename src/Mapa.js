c30f7171db5587169e30bd41bbfed3807b8c2dda
RafaelHenriques96 <aluno19072@ipt.pt>
2023-05-14 21:17:13 +0100

    Adição do Mapa
    Adição do Mapa

--- src/Mapa.js
+++ src/Mapa.js
@@ -1,10 +1,66 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {
+    TouchableOpacity,
+    SafeAreaView,
+    StyleSheet,
+    StatusBar,
+  } from 'react-native';
 
-export default function Mapa() {
-    return (
-        <View>
-        <Text>Mapa</Text>
-        </View>
-    );
-}
+  import{
+    WebView
+  } from 'react-native-webview';
+
+  import html_script from './html_script'
+
+  const App = () => {
+
+    
+  
+        return (
+
+        <>
+          <StatusBar barStyle="dark-content" />
+          <SafeAreaView style={styles.Container}>
+            <WebView source={{html: html_script }} style={styles.Webview} />
+            <View style={styles.ButtonArea}>
+              
+            </View>
+          </SafeAreaView>
+        </>
+      );
+    };
+    
+  
+  const styles = StyleSheet.create({
+    Container: {
+      flex:1,
+      padding: 10,
+      backgroundColor: 'grey'
+    
+    },
+    Webview: {
+      flex: 2,
+      
+    },
+    ButtonArea: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center'
+    },
+    Button: {
+      width: 80,
+      padding: 10,
+      borderRadius: 10,
+      backgroundColor: 'black',
+      alignItems: 'center'
+    },
+    ButtonText: {
+      color: 'white',
+      fontWeight: 'bold',
+      fontSize: 14,
+    }
+  });
+  
+  export default App;
